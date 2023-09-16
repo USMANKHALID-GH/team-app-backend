57e2870a352c39f8f6ca4d6b9418d95cf707effc
USMAN KHALID <66887206+USMANKHALID-GH@users.noreply.github.com>
2023-09-16 11:44:26 +0300

    address

--- src/main/java/com/zalisoft/teamapi/model/Address.java
+++ src/main/java/com/zalisoft/teamapi/model/Address.java
@@ -8,18 +8,25 @@ import javax.persistence.Embeddable;
 @Data
 @Embeddable
 public class Address {
+
     @Column(name = "country")
     private String country;
+
     @Column(name = "city")
     private String city;
+
     @Column(name = "town")
     private String town;
+
     @Column(name = "street_name")
     private String streetName;
+
     @Column(name = "street_code")
     private String streetCode;
+
     @Column(name = "house_no")
     private String houseNumber;
+
     @Column(name = "address_description")
     private String descriptions;
 }
