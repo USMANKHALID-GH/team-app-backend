e5902ba61807037c5d711abf2a49c5b0f4a6c358
USMAN KHALID <66887206+USMANKHALID-GH@users.noreply.github.com>
2023-09-19 15:57:29 +0300

    email api

--- src/main/java/com/zalisoft/teamapi/service/EmailService.java
+++ src/main/java/com/zalisoft/teamapi/service/EmailService.java
@@ -1,5 +1,10 @@
 package com.zalisoft.teamapi.service;
 
+import javax.mail.MessagingException;
+import java.io.UnsupportedEncodingException;
+
 public interface EmailService {
     void sendMessageToProjectManagersForUnfinishedProject();
+
+    void countUsersWithMoreThanTheGivenOccurrences() throws MessagingException, UnsupportedEncodingException;
 }
--- src/main/java/com/zalisoft/teamapi/service/impl/EmailServiceImpl.java
+++ src/main/java/com/zalisoft/teamapi/service/impl/EmailServiceImpl.java
@@ -1,7 +1,10 @@
 package com.zalisoft.teamapi.service.impl;
 
 import com.zalisoft.teamapi.model.Project;
+import com.zalisoft.teamapi.model.User;
+import com.zalisoft.teamapi.service.CautionService;
 import com.zalisoft.teamapi.service.EmailService;
+import com.zalisoft.teamapi.service.ParameterService;
 import com.zalisoft.teamapi.service.ProjectService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -16,7 +19,9 @@ import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
 import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -28,9 +33,15 @@ public class EmailServiceImpl  implements EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Autowired
+    private CautionService cautionService;
+
+    @Autowired
+    private ParameterService parameterService;
+
 
     public void sendMessageToProjectManagersForUnfinishedProject() {
-        List<Project> projectList=projectService.findUnfinishedProject();
+        List<Project> projectList=projectService.findUnfinishedProjectThatExceedDeadline();
         log.info("inside email----");
         projectList.stream()
                 .forEach(m-> {
@@ -46,6 +57,18 @@ public class EmailServiceImpl  implements EmailService {
     }
 
 
+    public void countUsersWithMoreThanTheGivenOccurrences() throws MessagingException, UnsupportedEncodingException {
+        log.info("inside email");
+            List<User> user= cautionService.countUsersWithMoreThanTheGivenOccurrences();
+           List<String> email= user.stream()
+                    .map(User::getEmail)
+                    .collect(Collectors.toList());
+           sendMail(parameterService.getMessageForLimitCaution(), email);
+           cautionService.deleteByUserId(user.stream().map(User::getId).collect(Collectors.toList()));
+
+
+    }
+
 
     private void sendMail(String message ,String receiver) throws MessagingException, UnsupportedEncodingException {
         log.info("mesage : {}   receiver : {}",message,receiver);
@@ -53,6 +76,17 @@ public class EmailServiceImpl  implements EmailService {
         helper.setText("WARNING");
         helper.setSubject("WARNING");
         helper.setTo(receiver);
+        helper.setFrom("loftyusman@gmail.com", "ZALISOFT");
+        helper.setText(message, true);
+        mailSender.send(helper.getMimeMessage());
+    }
+
+    private void sendMail(String message ,List<String> receiver) throws MessagingException, UnsupportedEncodingException {
+        log.info("mesage : {}   receiver : {}",message,receiver);
+        MimeMessageHelper helper = new MimeMessageHelper(mailSender.createMimeMessage());
+        helper.setText("WARNING");
+        helper.setSubject("WARNING");
+        helper.setTo(receiver.toArray(new String[receiver.size()]));
         helper.setFrom("loftyusman@gmail.com", "ZALISOFT");
         helper.setText(message, true);
         mailSender.send(helper.getMimeMessage());
