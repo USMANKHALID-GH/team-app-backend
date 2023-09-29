63337a791b1dee6d9e63bc305d367dd717e4915d
USMAN KHALID <66887206+USMANKHALID-GH@users.noreply.github.com>
2023-09-29 14:56:03 +0300

    json convertor added

--- src/main/java/com/zalisoft/teamapi/util/General.java
+++ src/main/java/com/zalisoft/teamapi/util/General.java
@@ -1,29 +1,50 @@
 package com.zalisoft.teamapi.util;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.zalisoft.teamapi.constant.GeneralConstant;
+import com.zalisoft.teamapi.dto.TeamDto;
 import com.zalisoft.teamapi.enums.ResponseMessageEnum;
 import com.zalisoft.teamapi.exception.BusinessException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Arrays;
-
+@Slf4j
 public class General {
 
 
 
-    static  void checkIfIsImage(MultipartFile file){
-        if(!isImage(file.getOriginalFilename())){
+   public static  void checkIfIsImage(String  file){
+        if(!isImage(file)){
             throw new BusinessException(ResponseMessageEnum.BACK_IMAGE_MSG_001);
+        }
+    }
+
+
+
+    public  static  <T> T  convertToJson(String json, Class<T> class_ ){
+
+        try {
+            ObjectMapper objectMapper=new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
+            return objectMapper.readValue(json, class_);
+        }catch (Exception e){
+            log.info("infor {}", e.getMessage());
+            throw new BusinessException(ResponseMessageEnum.BACK_JSON_CONVERTOR_MSG_001);
         }
 
     }
 
 
-    static boolean  isImage(String file){
+   private static boolean  isImage(String file){
      return Arrays.stream(GeneralConstant.IMAGE_EXTENSION)
               .anyMatch(s -> file.toLowerCase().endsWith(s));
 
         }
+
+
+
 
 
 }
